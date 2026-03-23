fb2b4d54242e2af0360b10d82e99d0e24714cbe4
Garett.Janulewicz <janulewiczgarett@gmail.com>
2026-03-23 13:48:21 -1000

    settings.py for deployment

--- hawaiitraveltips/settings.py
+++ hawaiitraveltips/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
