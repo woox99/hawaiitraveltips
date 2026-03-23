e7a4e38284708a35bb6e860c52734eae57d1e90d
Garett.Janulewicz <janulewiczgarett@gmail.com>
2026-03-22 17:00:45 -1000

    settings_local.py

--- hawaiitraveltips/settings.py
+++ hawaiitraveltips/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h5c(pb8qgkrg3^*^iai_!pb((1=ioio=zwbua99qo)!mp415z-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -111,3 +111,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Overide settings.py for development from settings_local.py
+try:
+    from .settings_local import *
+except ImportError:
+    pass
