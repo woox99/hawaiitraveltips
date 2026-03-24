842f3d22cdd3db0ff5c3c9bb878a35437e44ae11
Garett.Janulewicz <janulewiczgarett@gmail.com>
2026-03-23 14:07:42 -1000

    .env

--- hawaiitraveltips/settings.py
+++ hawaiitraveltips/settings.py
@@ -2,16 +2,20 @@
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h5c(pb8qgkrg3^*^iai_!pb((1=ioio=zwbua99qo)!mp415z-"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
